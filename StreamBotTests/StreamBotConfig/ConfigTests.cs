9b67096abc61a681a3e602de236def0190fa3885
Dominik Marchewicz <marchewiczd@gmail.com>
2021-03-07 14:15:25 +0100

    Added config tests

---
+++ StreamBotTests/StreamBotConfig/ConfigTests.cs
@@ -0,0 +1,32 @@
+﻿using StreamBotConfig.Configs;
+using Xunit;
+
+namespace StreamBotTests.StreamBotConfig
+{
+    public class ConfigTests
+    {
+        [Fact]
+        public void DeserializerDirectoryNotFound()
+        {
+            Config conf = Config.Deserialize("./NonExistentFolder1111/Config.xml");
+
+            Assert.Null(conf);
+        }
+
+        [Fact]
+        public void DeserializerFileNotFound()
+        {
+            Config conf = Config.Deserialize("./NonExistentConf.xml");
+
+            Assert.Null(conf);
+        }
+
+        [Fact]
+        public void DeserializeFileFound()
+        {
+            Config conf = Config.Deserialize("Config.xml");
+
+            Assert.NotNull(conf);
+        }
+    }
+}
